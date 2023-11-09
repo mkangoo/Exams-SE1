56623fb25495a81e05d9818b5f8662b73fc561e6
mkangoo <alexanderpavlov1408@gmail.com>
2023-11-09 03:11:13 +0300

    refactor: edits and addition of Array.from method

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,14 +1,8 @@
 // Return an array without duplicates
 
-duplicate = (value, N) => {
-  if (N <= 0) return [];
-  else {
-    res = [];
-    for (let i = 0; i < N; i++) {
-      res[i] = value;
-    }
-    return res;
-  }
+const duplicate = (value, n) => {
+  if (n <= 0) return [];
+  return Array.from({ length: n }, () => value);
 };
 
-require('../Tests/duplicate.js')(duplicate);
+require("../Tests/duplicate.js")(duplicate);
